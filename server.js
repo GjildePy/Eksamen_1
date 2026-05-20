3f144913d55f124a10ebbb941b356ee09ce8c8fb
Gjilde <gildeoliver07@gmail.com>
2026-05-20 15:01:25 +0200

    1

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path'); 
+const app = express();
+
+// Sørger for at serveren finner style.css og klarer å lese tekst som blir sendt
+app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true }));
+
+// Sender index.html med en gang noen åpner siden
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Mottak av rapporten (Svarer nå lydløst med status 200 OK)
+app.post('/rapporter', (req, res) => {
+    const melding = req.body.problemMelding;
+    
+    console.log("⚠️ NY RAPPORT MOTTATT:");
+    console.log(melding);
+    console.log("------------------------");
+    
+    res.sendStatus(200); // Forteller nettleseren at alt gikk bra
+});
+
+// Starter serveren på port 3000
+app.listen(3000, () => {
+    console.log("Serveren kjører på http://localhost:3000");
+});
